70b87d3ca570bdf7404aa11e3d09ef0f0389ed0d
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-07 15:47:47 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <h1>Moja Strona</h1>
+      </header>
+      <main>
+        <p>Witaj na mojej stronie!</p>
+      </main>
+      <footer>
+        <p>Stopka strony</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
