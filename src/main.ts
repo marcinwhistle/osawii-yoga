d677a4a20ee27030e5c1bbc70352c6ca05d347bb
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-08 09:36:05 +0200

    Add api into url for endpoints

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggerInterceptor());
+  app.setGlobalPrefix('api');
   await app.listen(8000);
 }
 bootstrap();
