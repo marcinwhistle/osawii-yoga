f2f43d97294704860aede1159a6b7b0c7630b06a
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-11 17:43:24 +0200

    Add Button for every product

--- client/src/components/views/ProductList/ProductList.js
+++ client/src/components/views/ProductList/ProductList.js
@@ -1,3 +1,4 @@
+import Button from '../../common/Button/Button';
 import styles from './ProductList.module.scss';
 
 const ProductList = () => {
@@ -6,17 +7,33 @@ const ProductList = () => {
       <div className={styles.row}>
         <div className={styles.col}>
           <img src="/images/1.jpg" alt="Product 1" />
+          <div className={styles.buttonGroup}>
+            <Button variant="outline">Szczegóły</Button>
+            <Button variant="outline">Dodaj do koszyka</Button>
+          </div>
         </div>
         <div className={styles.col}>
           <img src="/images/2.jpg" alt="Product 2" />
+          <div className={styles.buttonGroup}>
+            <Button variant="outline">Szczegóły</Button>
+            <Button variant="outline">Dodaj do koszyka</Button>
+          </div>
         </div>
       </div>
       <div className={styles.row}>
         <div className={styles.col}>
           <img src="/images/3.jpg" alt="Product 3" />
+          <div className={styles.buttonGroup}>
+            <Button variant="outline">Szczegóły</Button>
+            <Button variant="outline">Dodaj do koszyka</Button>
+          </div>
         </div>
         <div className={styles.col}>
           <img src="/images/4.jpg" alt="Product 4" />
+          <div className={styles.buttonGroup}>
+            <Button variant="outline">Szczegóły</Button>
+            <Button variant="outline">Dodaj do koszyka</Button>
+          </div>
         </div>
       </div>
     </div>
