bc31c61206eb66eed42e6a67b9d32d77473a0088
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-11 14:17:53 +0200

    Add Header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,22 @@ import Homepage from './components/layout/Homepage/Homepage';
 import Product from './components/views/Product/Product/Product';
 import Cart from './components/views/Product/Cart/Cart';
 import Order from './components/views/Order/Order';
+import Container from 'react-bootstrap/esm/Container';
 
 const App = () => (
   <Provider store={store}>
-    <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route exact path="/" element={<Homepage />} />
-          <Route exact path="/:product" element={<Product />} />
-          <Route exact path="/cart" element={<Cart />} />
-          <Route exact path="/order" element={<Order />} />
-        </Routes>
-      </MainLayout>
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <MainLayout>
+          <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route exact path="/:product" element={<Product />} />
+            <Route exact path="/cart" element={<Cart />} />
+            <Route exact path="/order" element={<Order />} />
+          </Routes>
+        </MainLayout>
+      </BrowserRouter>
+    </Container>
   </Provider>
 );
 
--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <Row className=" text-muted justify-content-center">
+        <Col className="mb-4" md="auto">
+          Osawii Yoga © 2023
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Footer;
