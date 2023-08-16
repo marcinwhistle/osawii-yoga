2c542ea751d0998dbbe509d70476af9976f9e37b
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-16 09:25:42 +0200

    Change reducer for products conf

--- client/src/redux/productsRedux.js
+++ client/src/redux/productsRedux.js
@@ -1,77 +1,32 @@
-import axios from 'axios';
 import { API_URL } from '../config';
 
 //selectors
-export const getProducts = ({ products }) => products;
-export const getRequest = ({ products }) => products.request;
-// actions
+export const getAllProducts = ({ products }) => products;
 
 // action name creator
 const reducerName = 'products';
-const createActionName = (name) => `app/${reducerName}/${name}`;
+const createActionName = (actionName) => `api/${reducerName}/${actionName}`;
 
-const START_REQUEST = createActionName('START_REQUEST');
-const END_REQUEST = createActionName('END_REQUEST');
-const ERROR_REQUEST = createActionName('ERROR_REQUEST');
+const UPDATE_PRODUCTS = createActionName('UPDATE_PRODUCTS');
 
-const LOAD_PRODUCTS = createActionName('LOAD_PRODUCTS');
+//action creators
+export const updateProducts = (payload) => ({ type: UPDATE_PRODUCTS, payload });
 
-export const startRequest = () => ({ type: START_REQUEST });
-export const endRequest = () => ({ type: END_REQUEST });
-export const errorRequest = () => ({ type: ERROR_REQUEST });
-
-export const loadProducts = () => ({ type: LOAD_PRODUCTS });
-
-/* THIUNKS */
-
-export const loadProductsRequest = () => {
-  return async (dispatch) => {
-    dispatch(startRequest());
-    try {
-      let res = await axios.get(`${API_URL}/products`);
-      dispatch(loadProducts(res.data));
-      console.log('Fetched products:', res.data);
-      dispatch(endRequest());
-    } catch (error) {
-      dispatch(errorRequest(error.message));
-    }
+export const fetchProducts = () => {
+  return (dispatch) => {
+    fetch(API_URL + 'products')
+      .then((res) => res.json())
+      .then((products) => dispatch(updateProducts(products)));
   };
 };
 
-/* INITIAL STATE */
-
-const initialState = {
-  data: [],
-  request: {
-    pending: false,
-    error: null,
-    success: null,
-  },
-};
-
-/* REDUCER */
-
-export default function reducer(statePart = initialState, action = {}) {
+const productsReducer = (statePart = [], action) => {
   switch (action.type) {
-    case LOAD_PRODUCTS:
-      return { ...statePart, data: [...action.payload] };
-    case START_REQUEST:
-      return {
-        ...statePart,
-        request: { pending: true, error: null, success: false },
-      };
-    case END_REQUEST:
-      return {
-        ...statePart,
-        request: { pending: false, error: null, success: true },
-      };
-    case ERROR_REQUEST:
-      return {
-        ...statePart,
-        request: { pending: false, error: action.error, success: false },
-      };
+    case UPDATE_PRODUCTS:
+      return [...action.payload];
     default:
-      console.log(statePart);
       return statePart;
   }
-}
+};
+
+export default productsReducer;
