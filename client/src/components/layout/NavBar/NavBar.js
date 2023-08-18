ca76f14e58fbce7e98adb0ff90e86e0dbc7a486b
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-18 13:29:30 +0200

    Add Link navigation instead NavLink

--- client/src/components/layout/NavBar/NavBar.js
+++ client/src/components/layout/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import NavBar from 'react-bootstrap/NavBar';
 import Nav from 'react-bootstrap/Nav';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const CustomNavBar = () => {
   return (
@@ -12,12 +12,14 @@ const CustomNavBar = () => {
       className="mt-4 mb-4 rounded"
     >
       <Container>
-        <NavBar.Brand href="/">Osawii Yoga</NavBar.Brand>
+        <NavBar.Brand as={Link} to="/">
+          Osawii Yoga
+        </NavBar.Brand>
         <Nav>
-          <Nav.Link as={NavLink} to="/" className="text-dark">
+          <Nav.Link as={Link} to="/" className="text-dark">
             Home
           </Nav.Link>
-          <Nav.Link as={NavLink} to="/cart" className="text-dark">
+          <Nav.Link as={Link} to="/cart" className="text-dark">
             Cart
           </Nav.Link>
         </Nav>
