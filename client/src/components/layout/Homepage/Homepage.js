93552ad8b5d9ca4d72ebabd83a9f9ace4364dfc3
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-18 15:28:28 +0200

    Style Footer and baner

--- client/src/components/layout/Homepage/Homepage.js
+++ client/src/components/layout/Homepage/Homepage.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ProductList from '../../views/ProductList/ProductList';
 import styles from './Homepage.module.scss';
 import Container from 'react-bootstrap/esm/Container';
+import { Col, Row } from 'react-bootstrap';
 
 const Homepage = () => {
   return (
     <>
       <div className={styles.backgroundImage}></div>
       <Container>
-        <h1>Odzyskaj swoją wewnętrzną równowagę:</h1>
+        <Row>
+          <Col className="text-center mt-4 mb-4">
+            <h1>Odzyskaj swoją wewnętrzną równowagę:</h1>
+          </Col>
+        </Row>
         <ProductList />
       </Container>
     </>
--- client/src/components/layout/NavBar/NavBar.js
+++ client/src/components/layout/NavBar/NavBar.js
@@ -1,30 +1,34 @@
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
-import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const CustomNavBar = () => {
   return (
-    <Navbar bg="light" variant="light" expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/" className="me-auto">
-          <h2>Osawii Yoga</h2>
-        </Navbar.Brand>
-      </Container>
+    <Navbar
+      bg="light"
+      variant="light"
+      expand="sm"
+      className="d-flex justify-content-between"
+    >
+      <Navbar.Brand as={Link} to="/" className="">
+        <h2>Osawii Yoga</h2>
+      </Navbar.Brand>
       <Navbar.Toggle />
-      <Nav className="me-auto">
-        <Nav.Link as={Link} to="/" className="h3 text-dark">
-          Home
-        </Nav.Link>
-        <Nav.Link as={Link} to="/cart" className="h3 text-dark">
-          Cart
-        </Nav.Link>
-        <Nav.Link as={Link} to="/cart" className="h3 text-dark">
-          <FontAwesomeIcon icon={faShoppingCart} />
-        </Nav.Link>
-      </Nav>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="">
+          <Nav.Link as={Link} to="/" className="h3 text-dark">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/cart" className="h3 text-dark">
+            Cart
+          </Nav.Link>
+          <Nav.Link as={Link} to="/cart" className="h3 text-dark">
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
