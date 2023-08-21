39b6172a026bce2231adcbc1745c8a96bb606340
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-21 16:35:06 +0200

    Add cart quantity calculation

--- client/src/components/views/Product/Cart/Cart.js
+++ client/src/components/views/Product/Cart/Cart.js
@@ -4,19 +4,33 @@ import { getAll } from '../../../../redux/cartRedux';
 
 const Cart = () => {
   const dispatch = useDispatch();
+  const [totalPrice, setTotalPrice] = useState(0);
   const cartProducts = useSelector(getAll);
+
+  const calculateCartTotal = (cartProducts) => {
+    let subTotal = 0;
+    for (let i = 0; i < cartProducts.length; i++) {
+      subTotal += cartProducts[i].price * cartProducts[i].quantity;
+    }
+    return subTotal;
+  };
+
+  useEffect(() => {
+    const total = calculateCartTotal(cartProducts);
+    setTotalPrice(total);
+  }, [cartProducts]);
 
   return (
     <>
-      <div>Cart</div>
+      <h1>Podsumowanie koszyka</h1>
       {cartProducts.map((product) => (
         <div key={product.id}>
           <p>{product.name}</p>
           <p>{product.price}</p>
-          <p>{product.description}</p>
-          <p>Amount: {product.quantity}</p>
+          <p>Ilość: {product.quantity}</p>
         </div>
       ))}
+      <h2>Wartośc koszyka: {totalPrice}</h2>
     </>
   );
 };
