f76bce15a735caf93282624d6f4a3dd9eb8f348b
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-22 13:10:54 +0200

    Add styles for product

--- client/src/components/views/Product/Product/Product.js
+++ client/src/components/views/Product/Product/Product.js
@@ -44,25 +44,25 @@ const Product = () => {
                 src={`${process.env.PUBLIC_URL}/images/${productData.image}`}
                 alt={productData.name}
               />
-              <div className={styles.infoContainer}>
-                <p className={styles.price}>
-                  <strong>Cena: </strong> {productData.price}
-                </p>
-                <Button variant="primary" onClick={handleAddToCartClick}>
-                  Add to cart
-                </Button>
-                <input
-                  className={styles.input}
-                  type="number"
-                  min="1"
-                  value={quantity}
-                  onChange={(e) => setQuantity(e.target.value)}
-                />
-              </div>
             </div>
             <p className={styles.description}>{productData.description}</p>
           </Col>
         </Row>
+        <div className={styles.infoContainer}>
+          <p className={styles.price}>
+            <strong>Cena: </strong> {productData.price}
+          </p>
+          <Button variant="primary" onClick={handleAddToCartClick}>
+            Add to cart
+          </Button>
+          <input
+            className={styles.input}
+            type="number"
+            min="1"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+        </div>
         <Row>
           {additionalImages.map((image, index) => (
             <Col key={index} className=" d-flex">
